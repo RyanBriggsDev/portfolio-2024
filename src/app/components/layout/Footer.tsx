3b78cf26c1a9343485085f8352eb564c071483da
Ryan Briggs <r@ryanbriggs.dev>
2024-08-15 22:19:46 +0100

    added: footer section and contect

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-black text-white py-6">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:mb-0">
+            <a
+              href="https://github.com/ryanbriggsdev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-blue-300 transition-colors duration-300"
+            >
+              GitHub
+            </a>
+          </div>
+          <div className="text-center md:text-right">
+            <p>&copy; {currentYear} Ryan Briggs. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
