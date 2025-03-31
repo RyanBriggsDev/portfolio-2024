b1632689ae48d33f4bf1b5442ce5d6a1d9dcf299
Ryan Briggs <r@ryanbriggs.dev>
2025-03-31 18:08:39 +0100

    feat: adding thatworks to experience

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,6 +1,7 @@
 import ContentSection from "./layout/ContentSection";
 import AccordionSection from "./AccordionSection";
 import RCALogo from "../assets/images/icons/rca_logo.svg";
+import ThatWorksLogo from "../assets/images/icons/thatworks.jpeg";
 
 type ExperienceCategory = {
   title: string;
@@ -17,13 +18,25 @@ type ExperienceCategory = {
 
 const experience: ExperienceCategory[] = [
   {
+    title: "Expert Shopify Developer",
+    company: "That Works",
+    description: "Project Development Optimising Themes for Speed & Conversion",
+    employmentType: "Full-time",
+    startDate: "Nov 2024",
+    endDate: "Present",
+    location: "Anywhere",
+    workArrangement: "Remote",
+    skills: ["Liquid", "Tailwind CSS", "JavaScript", "Git", "Vue.js"],
+    logo: ThatWorksLogo.src,
+  },
+  {
     title: "Junior Front-End Developer",
     company: "Rainy City Agency",
     description: "Award-Winning Shopify Plus Agency",
     employmentType: "Full-time",
     startDate: "Jul 2023",
-    endDate: "Present",
-    location: "Manchester, United Kingdom",
+    endDate: "Nov 2024",
+    location: "Manchester, UK",
     workArrangement: "Hybrid",
     skills: ["Liquid", "Tailwind CSS", "JavaScript", "Git", "React"],
     logo: RCALogo.src,
